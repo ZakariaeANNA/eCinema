1e1f8c9f6c3391c3e9bafa30845a891f3c08b953
ELBARAE <a.barae@hotmail.com>
2022-10-19 13:42:49 +0100

    genre

--- src/main/java/com/hendisantika/controller/GenreController.java
+++ src/main/java/com/hendisantika/controller/GenreController.java
@@ -2,17 +2,28 @@ package com.hendisantika.controller;
 
 import org.springframework.stereotype.Controller;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hendisantika.entity.Genre;
